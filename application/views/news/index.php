bda2b8c1b416c19357d24e138b725f672ffafa88
Kurmanbek <kumakaraev01@gmail.com>
2020-06-09 23:35:53 -0700

    finally news section finished

--- application/views/news/index.php
+++ application/views/news/index.php
@@ -24,15 +24,13 @@
                         <h1>
                             <a href="<?php echo site_url('news/'.$news_item['slug']); ?>"><?php echo $news_item['title']; ?></a>
                         </h1>
+                        <p class="text">
+                            <i class="fa fa-user"><?php echo $news_item['text']; ?></a>
+                        </p>
+                        <hr>
                         <p class="lead">
                             <i class="fa fa-user"></i> by <a href=""><?= isset($user) ? $user['name'] : 'Unknown' ?></a>
                         </p>
-                        <hr>
-                        <p>
-                            <i class="fa fa-calendar"></i> Posted on <?= isset($user) ? $news_item['formatted_date'] : 'Unknown' ?>
-                        </p>
-                        <hr>
-                        <br/>
                 </div>
             <?php endforeach; ?>
         </div>
